2a416c9aa33a255d3d11b74e70c5b97d0ac720d3
kavindisathsarani <dksmanimendra0@gmail.com>
2025-07-05 17:02:55 +0530

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,32 @@
 import express, {Express, Request, Response} from "express";
 import productRoutes from "./routes/product.routes"
+import cors from "cors"
 
 // 1. Initialize the express app
 const app = express();
 
 //2.Define Middlewares
 app.use(express.json());
+const allowedOrigins=[
+    "http://localhost:5173"
+
+];
+
+// app.use(cors()); //Enable/Allow CORS here
+
+const corsOptions={
+ origin:(origin:string|undefined,
+         callback: (err: Error | null, origin?: boolean | undefined) => void) => {
+     if(!origin || allowedOrigins.includes(origin)){
+         callback(null,true);
+     } else {
+        callback(new Error("Not Allowed by CORS"))
+     }
+ }
+}
+
+app.use(cors());
+
 
 app.use("/api/products", productRoutes)
 
