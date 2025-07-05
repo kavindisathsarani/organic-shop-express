5b91b37e76fd9f6de2c69edea6eee58f940a0650
kavindisathsarani <dksmanimendra0@gmail.com>
2025-07-05 11:15:01 +0530

    model created

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, {Express, Request, Response} from "express";
+import productRoutes from "./routes/product.routes"
 
 // 1. Initialize the express app
 const app = express();
 
 //2.Define Middlewares
 app.use(express.json());
-app.get("/", (req:Request, res:Response) => {
-    res.send("Hello world hi!");
-})
+
+app.use("api/products", productRoutes)
+
+//Define a simple HTTP GET request
+// app.get("/", (req:Request, res:Response) => {
+//     console.log(req.body)
+//     res.send("Hello world hi!");
+// })
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from "./app";
+import dotenv from "dotenv"
+
+dotenv.config(); //Load environment variables from a .env file into process.env
 
 // Define the application port
-const port = 3000;
+const port = process.env.PORT || 3000; //Access the value of the PORT environment variable
 
 //4.Instructs the express app to listen on port 3000
 app.listen(port, () => {
