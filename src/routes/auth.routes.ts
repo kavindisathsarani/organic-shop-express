96e7215db32510e464ee84a4367eec2f0d8ae790
kavindisathsarani <dksmanimendra0@gmail.com>
2025-07-12 17:03:54 +0530

    json token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
+import {authenticateUser} from "../controllers/auth.controller";
 
 const authRouter:Router=Router();
-authRouter.post("/login",);
+authRouter.post("/login",authenticateUser);
 export default authRouter;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,12 @@
 import {User} from "../model/user.model";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const JWT_SECRET= process.env.JWT_SECRET as string;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
+
+const refreshTokens = new Set<string>();
 
 const adminUser: User = {
     id: 1,
@@ -24,10 +30,24 @@ userList.push(customerUser);
 export const authenticateUser = (username: string, password: string) => {
     const existingUser: User | undefined = userList.find(user => user.username === username);
 
-    let isValidPassword = bcrypt.compareSync(password, <string>existingUser?.password);
+    let isValidPassword = undefined != existingUser && bcrypt.compareSync(password,existingUser.password);
     if (!existingUser || !isValidPassword) {
         return null;
     }
 
-    jwt
+    const accessToken = jwt.sign({
+        id: existingUser.id,
+        username: existingUser.username,
+        role: existingUser.role
+    },JWT_SECRET,{expiresIn:"5m"}); //30 min or 1h is recommended for this. don't exceed 1h timeline
+
+    const refreshToken = jwt.sign({
+        username: existingUser.username
+    },REFRESH_TOKEN_SECRET,{
+        expiresIn:"7d"
+    });
+
+    refreshTokens.add(refreshToken);
+    return {accessToken, refreshToken};
+
 }
